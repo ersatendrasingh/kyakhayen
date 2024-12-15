0f8bc67a5e67692965f6ad808f7af136be3401f5
Satendra <satendra.singh@nutriwel.co>
2024-12-15 23:16:17 +0530

    update config

--- components/user-personalization/cuisines.tsx
+++ components/user-personalization/cuisines.tsx
@@ -1,19 +1,16 @@
 "use client";
 
 import Image from "next/image";
-import { useEffect, useState } from "react";
-
+import { useEffect, useState, useCallback } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Navigation, Mousewheel, Keyboard } from "swiper/modules";
+import { Navigation, Mousewheel, Keyboard, Virtual } from "swiper/modules";
 
 import { Cuisines as CuisinesType } from "@prisma/client";
-
 import useWindowSize from "@/hooks/use-window-size";
 import { cn } from "@/lib/utils";
-
 import { PersonalizationSkelton } from "@/components/user-personalization/personalization-skelton";
 
 interface CuisinesProps {
@@ -21,6 +18,7 @@ interface CuisinesProps {
   title: string;
   setIsFormValid: (isValid: boolean) => void;
 }
+
 const getSavedCuisines = (): string[] => {
   try {
     if (typeof window !== "undefined") {
@@ -43,6 +41,7 @@ const Cuisines = ({ cuisines, title, setIsFormValid }: CuisinesProps) => {
   );
   const [loading, setLoading] = useState(true);
   const { width } = useWindowSize();
+
   useEffect(() => {
     setLoading(false);
   }, []);
@@ -51,39 +50,44 @@ const Cuisines = ({ cuisines, title, setIsFormValid }: CuisinesProps) => {
     const existingUserData = JSON.parse(
       localStorage.getItem("userData") || "{}"
     );
-
-    const updatedUserData = {
-      ...existingUserData,
-      cuisines: selectedCuisines,
-    };
-
+    const updatedUserData = { ...existingUserData, cuisines: selectedCuisines };
     localStorage.setItem("userData", JSON.stringify(updatedUserData));
     setIsFormValid(selectedCuisines.length > 0);
   }, [selectedCuisines, setIsFormValid]);
 
-  const toggleCuisineSelection = (cuisineId: string) => {
-    if (selectedCuisines.includes(cuisineId)) {
-      setSelectedCuisines(selectedCuisines.filter((id) => id !== cuisineId));
-    } else {
-      setSelectedCuisines([...selectedCuisines, cuisineId]);
-    }
-  };
+  const toggleCuisineSelection = useCallback(
+    (cuisineId: string) => {
+      if (selectedCuisines.includes(cuisineId)) {
+        setSelectedCuisines(selectedCuisines.filter((id) => id !== cuisineId));
+      } else {
+        setSelectedCuisines([...selectedCuisines, cuisineId]);
+      }
+    },
+    [selectedCuisines]
+  );
 
   const isCuisineSelected = (cuisineId: string) =>
-    selectedCuisines?.includes(cuisineId) || false;
+    selectedCuisines.includes(cuisineId);
+
   if (loading) {
     return <PersonalizationSkelton />;
   }
 
   const isMobile = width !== undefined && width <= 767;
+
+  // Generate an array of visible items for the swiper (only the items that should be visible initially)
+  const visibleCuisines = isMobile
+    ? cuisines.slice(0, 3)
+    : cuisines.slice(0, 9);
+
   return (
     <div>
-      <h1 className="text-2xl  xl:text-3xl mb-3 font-semibold transition-all duration-1000 ease-in-out transform animate-slide-in text-websecondary">
+      <h1 className="text-2xl xl:text-3xl mb-3 font-semibold transition-all duration-1000 ease-in-out transform animate-slide-in text-websecondary">
         {title}
       </h1>
       <div
         className={cn(
-          "w-full  md:max-w-screen-xl flex items-center justify-center",
+          "w-full md:max-w-screen-xl flex items-center justify-center",
           isMobile && "max-w-[360px]"
         )}
       >
@@ -95,7 +99,8 @@ const Cuisines = ({ cuisines, title, setIsFormValid }: CuisinesProps) => {
           navigation={true}
           mousewheel={true}
           keyboard={true}
-          modules={[Navigation, Mousewheel, Keyboard]}
+          modules={[Navigation, Mousewheel, Keyboard, Virtual]} // Include the Virtual module for dynamic rendering
+          virtual={{ slides: visibleCuisines }} // Use virtual slides based on visible items
         >
           {cuisines.map((cuisine) => (
             <SwiperSlide
@@ -114,15 +119,16 @@ const Cuisines = ({ cuisines, title, setIsFormValid }: CuisinesProps) => {
                   width={112}
                   height={112}
                   className="rounded-full"
+                  loading="lazy" // Enable lazy loading for images
                 />
                 <span
                   className={cn(
-                    "absolute inset-0 bg-black  opacity-40 rounded-full transition-opacity duration-300",
+                    "absolute inset-0 bg-black opacity-40 rounded-full transition-opacity duration-300",
                     isCuisineSelected(cuisine.id) &&
                       "bg-red-500 opacity-100 text-white"
                   )}
                 ></span>
-                <span className="absolute inset-0  flex items-center justify-center text-white  py-2 transition-transform duration-300 transform translate-y-0 group-hover:translate-y-1">
+                <span className="absolute inset-0 flex items-center justify-center text-white py-2 transition-transform duration-300 transform translate-y-0 group-hover:translate-y-1">
                   {cuisine.title}
                 </span>
               </div>
